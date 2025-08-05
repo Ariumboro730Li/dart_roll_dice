d5cb7d4b97f544eebd46cbf496ae9ca27e78cc49
Ari Umboro Seno <ariumboroseno@gmail.com>
2025-08-05 10:17:46 +0700

    add box dexoration and style

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() {
 
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 22, 37, 123),
-        body: Center(
-          child: Text("Hello World"),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.red, Colors.yellow, Colors.green],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft
+            ),
+          ),
+          child: Center(
+            child: Text(
+              "Hello World", 
+              style: TextStyle(
+                fontSize: 40, 
+                color: Colors.black87, 
+                fontFamily: "Times New Roman"
+              ),
+            ),
+          ),
         )
       )
     )
