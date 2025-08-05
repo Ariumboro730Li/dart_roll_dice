b08e67abc89c1ff4d72d0a780e6087959a96ba4a
Ari Umboro Seno <ariumboroseno@gmail.com>
2025-08-05 11:08:38 +0700

    added class GradientContainer

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
-  runApp(MaterialApp(
-    home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.red, Colors.yellow, Colors.green],
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft
-            ),
-          ),
-          child: Center(
-            child: Text(
-              "Hello World", 
-              style: TextStyle(
-                fontSize: 40, 
-                color: Colors.black87, 
-                fontFamily: "Times New Roman"
-              ),
-            ),
-          ),
-        )
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(),
       )
-    )
+    ),
   );
+}
+
+// This is the main application widget.
+class GradientContainer extends StatelessWidget {
+  // GradientContainer({key}): super(key: key);
+  const GradientContainer({super.key});
+
+  @override
+  /// Builds a Container widget with a gradient background
+  /// that transitions from red, to yellow, to green.
+  ///
+  /// The gradient begins at the top right of the container
+  /// and ends at the bottom left.
+  ///
+  /// The Container also has a child Text widget that is
+  /// centered in the Container and displays the text
+  /// "Hello World from Flutter yeah yeahh" in black
+  /// 40-point font with a font family of "Times New Roman".
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.red, Colors.yellow, Colors.green],
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+        ),
+      ),
+      child: Center(
+        child: Text(
+          "Hello World from Flutter yeah yeahh",
+          style: TextStyle(
+            fontSize: 40,
+            color: Colors.black87,
+            fontFamily: "Times New Roman",
+          ),
+        ),
+      ),
+    );
+  }
 }
