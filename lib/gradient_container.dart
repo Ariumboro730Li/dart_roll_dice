df13fc16fe76488d8b9028da0f7dec84dae2c2f8
Ari Umboro Seno <ariumboroseno@gmail.com>
2025-08-05 11:53:15 +0700

    const variable

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -6,9 +6,17 @@ import 'package:roll_dice_app/styled_text.dart';
 // For example, the gradient can be made to flow from the bottom right
 // to the top left by setting 'startAlignment' to Alignment.bottomRight
 // and 'endAlignment' to Alignment.topLeft.
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomCenter;
+// !! var startAlignment = Alignment.topLeft;
+// !! var endAlignment = Alignment.bottomCenter;
 
+// 'const' is used here to make it clear that the values of
+// 'startAlignment' and 'endAlignment' are not intended to be changed.
+// Using 'const' here has the added benefit of being able to detect if
+// the values of 'startAlignment' and 'endAlignment' are accidentally
+// changed, since Dart will throw a compile-time error if a 'const'
+// variable is reassigned.
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomCenter;
 
 
 // This is the main application widget.
@@ -28,7 +36,7 @@ class GradientContainer extends StatelessWidget {
   /// 40-point font with a font family of "Times New Roman".
   Widget build(context) {
     // !! Debugging
-    startAlignment = Alignment.topRight;
+    // startAlignment = Alignment.topRight;
     // debugPrint(startAlignment.toString());
     // throw Exception('dd() called - stopping execution');
 
