af83206bc199501813615590063594ac38d85e1c
Ari Umboro Seno <ariumboroseno@gmail.com>
2025-08-05 11:51:19 +0700

    var variable

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:roll_dice_app/styled_text.dart';
 
+// The start and end of the gradient are specified as Alignment objects.
+// The variables are mutable so that they can be changed at runtime.
+// For example, the gradient can be made to flow from the bottom right
+// to the top left by setting 'startAlignment' to Alignment.bottomRight
+// and 'endAlignment' to Alignment.topLeft.
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomCenter;
+
+
+
 // This is the main application widget.
 class GradientContainer extends StatelessWidget {
-  // GradientContainer({key}): super(key: key);
+  // GradientContainer({key}): super(key: key);  
   const GradientContainer({super.key});
-
   @override
   /// Builds a Container widget with a gradient background
   /// that transitions from red, to yellow, to green.
@@ -18,12 +27,17 @@ class GradientContainer extends StatelessWidget {
   /// "Hello World from Flutter yeah yeahh" in black
   /// 40-point font with a font family of "Times New Roman".
   Widget build(context) {
+    // !! Debugging
+    startAlignment = Alignment.topRight;
+    // debugPrint(startAlignment.toString());
+    // throw Exception('dd() called - stopping execution');
+
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [Colors.red, Colors.yellow, Colors.green],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomLeft,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: Center(
