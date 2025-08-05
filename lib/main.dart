c0b48d0fa359747ac3f15f92ff5d61907328d234
Ari Umboro Seno <ariumboroseno@gmail.com>
2025-08-05 09:17:08 +0700

    scaffold with bg

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 
 void main() {
 
-  runApp(const MaterialApp(home: Text('Hello World')));
-
+  runApp(const MaterialApp(
+    home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 22, 37, 123),
+        body: Center(
+          child: Text("Hello World"),
+        )
+      )
+    )
+  );
 }
